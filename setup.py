bee1330d73c509497bc2931d740025a6e6152b52
prmiles <prmiles@ncsu.edu>
2017-10-20 06:50:23 -0400

    added setup.py
    - need to add a list of required packages to ensure installation works out of the box

---
+++ setup.py
@@ -0,0 +1,12 @@
+from setuptools import setup
+setup(
+    name='pymcmcstat',
+    version='0.1',
+    description='A library to perform mcmc simulations',
+    url='https://github.com/prmiles/pymcmcstat',
+    author='prmiles',
+    author_email='prmiles@ncsu.edu',
+    license='NCSU',
+    packages=['pymcmcstat'],
+    zip_safe=False,
+)
