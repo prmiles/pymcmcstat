3cfa754b2cafa79e885fe97fdf3ea6203672531b
prmiles <prmiles@ncsu.edu>
2017-11-08 12:10:39 -0500

    added nordf function
    - standard normal cumulative distribution

--- pymcmcstat/generalfunctions.py
+++ pymcmcstat/generalfunctions.py
@@ -6,6 +6,10 @@ Created on Fri Oct  6 16:50:08 2017
 @author: prmiles
 """
 
+from __future__ import division
+import math
+import numpy as np
+
 def less_than_or_equal_to_zero(x):
     return (x<=0)
 
@@ -20,7 +24,6 @@ def message(verbosity, level, printthis):
         print(printthis)
         
 def is_semi_pos_def_chol(x):
-    import numpy as np
     c = None
     try:
         c = np.linalg.cholesky(x)
@@ -38,3 +41,11 @@ def display_dictionary(dictionary):
 #            print('{} = {}\n' for item in attrs.items())
 #            print ', '.join("%s: %s" % item for item in attrs.items())
     print ''.join('%s = %s\n' % item for item in attrs.items())
+    
+def nordf(x, mu = 0, sigma2 = 1):
+    # NORDF the standard normal (Gaussian) cumulative distribution.
+    # NORPF(x,mu,sigma2) x quantile, mu mean, sigma2 variance
+    # Marko Laine <Marko.Laine@Helsinki.FI>
+    # $Revision: 1.5 $  $Date: 2007/12/04 08:57:00 $
+    # adapted for Python by Paul Miles, November 8, 2017
+    return 0.5 + 0.5*math.erf((x-mu)/math.sqrt(sigma2)/math.sqrt(2));
