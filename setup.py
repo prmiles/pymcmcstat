1dfc48da8fafc3529adce62dff779424ce8aa80d
prmiles <prmiles@ncsu.edu>
2017-11-10 11:34:16 -0500

    updated version number
    - include bug update

--- setup.py
+++ setup.py
@@ -1,7 +1,7 @@
 from setuptools import setup
 setup(
     name='pymcmcstat',
-    version='0.4dev',
+    version='0.4.1dev',
     description='A library to perform mcmc simulations',
     url='https://github.com/prmiles/pymcmcstat',
     author='Paul Miles',
