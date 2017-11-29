fixed bug in nbatch calculation
- nbatch should default to the length of N0, not N0 itself

pymcmcstat/classes.py
@@ -151,7 +151,7 @@ class Model:
         
         
         if nbatch is None:
-            self.nbatch = N0
+            self.nbatch = len(N0)
         else:
             self.nbatch = np.array([nbatch])
             