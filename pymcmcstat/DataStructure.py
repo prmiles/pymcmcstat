a85586ca87b58944e8b2276328b183e98bf30da8
prmiles <prmiles@ncsu.edu>
2018-01-17 17:13:19 -0500

    updated DataStructure class to be easier for user
    - A list is pseudo analagous to a cell array in Matlab, which allows for distinct data sets to have different sizes.  This allows for easier data manipulation as distinct data sets (requiring differenct error variances) can simply occupy a single element of a list.

--- pymcmcstat/DataStructure.py
+++ pymcmcstat/DataStructure.py
@@ -5,6 +5,8 @@ Created on Wed Jan 17 09:03:37 2018
 
 @author: prmiles
 """
+# import required packages
+import numpy as np
 
 # -------------------------
 # Define data structure
@@ -15,9 +17,35 @@ class DataStructure:
         self.ydata = [] # initialize list
         self.n = [] # initialize list - number of data points
         self.weight = [] # initialize list - weight of data set
-        self.udobj = [] # user defined object
+        self.user_defined_object = [] # user defined object
         
-    def add_data_set(self, x, y, n = None, weight = 1, udobj = 0):
+#        self.xdata = [] # initialize list
+#        self.ydata = [] # initialize list
+#        self.n = [] # initialize list - number of data points
+#        self.weight = [] # initialize list - weight of data set
+#        self.user_defined_object = [] # user defined object
+#        
+               
+        
+#    def add_empty_data_batch(self, initial_batch_flag = False):
+#        # Check if data already initialized
+#        if initial_batch_flag is True:
+#            self.batch = []
+#            
+##        self.batch.append(BasicDataStructure())
+#        
+##    def add_empty_data_batch(self, initial_batch_flag = 1):
+##        # Check if data already initialized
+##        if initial_batch_flag is None:
+##            self.data = []
+##            
+##        self.data.append(DataStructure())
+#        
+#        self.batch.append({'xdata': None, 'ydata': None, 'n': None,
+#                                'weight': None, 'user_defined_object': None})
+        
+    
+    def add_data_set(self, x, y, n = None, weight = 1, user_defined_object = 0):
         # append new data set
         self.xdata.append(x)
         self.ydata.append(y)
@@ -30,4 +58,18 @@ class DataStructure:
         
         self.weight.append(weight)
         # add user defined objects option
-        self.udobj.append(udobj)
+        self.user_defined_object.append(user_defined_object)
+        
+    def check_data_type(self, xy):
+        
+        if isinstance(xy, np.ndarray):
+            str('all is well')
+            
+        return True
+    
+    def get_number_of_batches(self):
+        return np.array([len(self.n)])
+    
+    def get_number_of_observations(self):
+        n = np.sum(self.n)
+        return np.array([n])
