b035b0104632f1fea7d8b3b56338176324d6a58a
prmiles <prmiles@ncsu.edu>
2018-01-17 17:15:13 -0500

    updated ModelParameters class
    - still working on streamlining the workflow and the accessiblity of values to the user

--- pymcmcstat/ModelParameters.py
+++ pymcmcstat/ModelParameters.py
@@ -8,102 +8,74 @@ Created on Wed Jan 17 09:13:03 2018
 # import required packages
 import numpy as np
 import math
+import sys
 
-class Parameters:
+class ModelParameters:
     def __init__(self):
-#        self.parameters = [] # initialize list
         self.parameters = [] # initialize list
         self.label = 'MCMC model parameters'
         
-    def add_parameter(self, name, theta0, minimum = -np.inf,
-                      maximum = np.inf, mu = np.zeros([1]), sig = np.inf,
+    def add_model_parameter(self, name, theta0, minimum = -np.inf,
+                      maximum = np.inf, prior_mu = np.zeros([1]), prior_sigma = np.inf,
                       sample = None, local = 0):
         
         # append dictionary element
         self.parameters.append({'name': name, 'theta0': theta0, 'minimum': minimum,
-                                'maximum': maximum, 'mu': mu, 'sig': sig,
+                                'maximum': maximum, 'prior_mu': prior_mu, 'prior_sigma': prior_sigma,
                                 'sample': sample, 'local': local})
-#        self.parameters.append([name, theta0, minimum, maximum, mu, sig, 
-#                          sample, local])
-        
-#        parameter = [name, theta0, minimum, maximum, mu, sig, 
-#                          sample, local]
-        
-#        self.parameters.append(parameter)
     
-    
-    def openparameterstructure(self, params, nbatch):
-        # NEED TO ADD COMPATIBILITY WITH HYPERPARAMETERS
+    def openparameterstructure(self, nbatch):
         
         # unpack input object
-        parameters = params.parameters
+        parameters = self.parameters
         npar = len(parameters)
         
         # initialize arrays - as lists and numpy arrays (improved functionality)
-        names = []
-        value = np.zeros(npar)
-        parind = np.ones(npar, dtype = int)
-        local = np.zeros(npar)
-        upp = np.ones(npar)*np.inf
-        low = -np.ones(npar)*np.inf
-        thetamu = np.zeros(npar)
-        thetasig = np.ones(npar)*np.inf
-        
-        nhpar = 0 # number of hyper parameters
-        
+        self.names = []
+        self.initial_value = np.zeros(npar)
+        self.parind = np.ones(npar, dtype = int)
+        self.local = np.zeros(npar)
+        self.upper_limits = np.ones(npar)*np.inf
+        self.lower_limits = -np.ones(npar)*np.inf
+        self.thetamu = np.zeros(npar)
+        self.thetasigma = np.ones(npar)*np.inf
+                
         # scan for local variables
         ii = 0
         for kk in range(npar):
             if parameters[kk]['sample'] is not None:
                 if parameters[kk]['local'] != 0:
-                    if parameters[kk]['local'] == 2:
-                        nhpar += 1 # add hyper parameter
-                        
-                    local[ii:(ii+nbatch-1)] = range(0,nbatch)
+                    self.local[ii:(ii+nbatch-1)] = range(0,nbatch)
                     npar = npar + nbatch - 1
                     ii = ii + nbatch - 1
-                    
-                    # add functionality for hyper parameters
-    #                            for k=2:7
-    #                if parstruct{i}{8}==2 & (k==5|k==6)
-    #                    if not(length(parstruct{i}{k})==1|length(parstruct{i}{k})==2)
-    #                        error(sprintf('Error in hyper parameters for %s',parstruct{i}{1}))
-    #                    end
-    #                else
-    #                    if length(parstruct{i}{k})~=nbatch
-    #                        if length(parstruct{i}{k})==1
-    #                            parstruct{i}{k} = parstruct{i}{k}*ones(1,nbatch);
-    #                        else
-    #                            error(sprintf('Not enough values for %s',parstruct{i}{1}))
+
             ii += 1 # update counter
             
          
         ii = 0
         for kk in range(npar):
-            if local[ii] == 0:
-                names.append(parameters[kk]['name'])
-                value[ii] = parameters[kk]['theta0']
+            if self.local[ii] == 0:
+                self.names.append(parameters[kk]['name'])
+                self.initial_value[ii] = parameters[kk]['theta0']
                 
                 # default values defined in "Parameters" class in classes.py
                 # define lower limits
-                low[ii] = parameters[kk]['minimum']
+                self.lower_limits[ii] = parameters[kk]['minimum']
                 # define upper limits
-                upp[ii] = parameters[kk]['maximum']
+                self.upper_limits[ii] = parameters[kk]['maximum']
                 # define prior mean
-                thetamu[ii] = parameters[kk]['mu']
-                if np.isnan(thetamu[ii]):
-                    thetamu[ii] = value[ii]
+                self.thetamu[ii] = parameters[kk]['prior_mu']
+                if np.isnan(self.thetamu[ii]):
+                    self.thetamu[ii] = self.value[ii]
                 # define prior standard deviation
-                thetasig[ii] = parameters[kk]['sig']
-                if thetasig[ii] == 0:
-                    thetasig[ii] = np.inf
+                self.thetasigma[ii] = parameters[kk]['prior_sigma']
+                if self.thetasigma[ii] == 0:
+                    self.thetasigma[ii] = np.inf
                     
             ii += 1 # update counter
             
         # make parind list of nonzero elements
-        parind = np.flatnonzero(parind)
-                
-        return names, value, parind, local, upp, low, thetamu, thetasig, npar
+        self.parind = np.flatnonzero(self.parind)
 
     def display_parameter_settings(self, parind, names, value, low, upp, thetamu, 
                                thetasig, noadaptind):
@@ -131,9 +103,10 @@ class Parameters:
                   value[parind[ii]], low[parind[ii]], upp[parind[ii]],
                   thetamu[parind[ii]], thetasig[parind[ii]], h2, st))
                 
-    def results2params(self, results, params, use_local = 1):
+    def results_to_params(self, results, use_local = 1):
     
         # unpack results dictionary
+        parameters = self.parameters
         parind = results['parind']
         names = results['names']
         local = results['local']
@@ -145,14 +118,38 @@ class Parameters:
             else:
                 name = names[ii]
     
-            for kk in range(len(params.parameters)):
-                if name == params.parameters[kk]['name']:
+            for kk in range(len(parameters)):
+                if name == parameters[kk]['name']:
                     # change NaN prior mu (=use initial) to the original initial value
-                    if np.isnan(params.parameters[kk]['mu']):
-                        params.parameters[kk]['mu'] = params[kk]['theta0']
+                    if np.isnan(parameters[kk]['mu']):
+                        parameters[kk]['mu'] = parameters[kk]['theta0']
                         
                     # only change if parind = 1 in params (1 is the default)
-                    if params.parameters[kk]['sample'] == 1 or params.parameters[kk]['sample'] is None:
-                        params.parameters[kk]['theta0'] = theta[parind[ii]]
+                    if parameters[kk]['sample'] == 1 or parameters[kk]['sample'] is None:
+                        parameters[kk]['theta0'] = theta[parind[ii]]
+    
+    def check_initial_values_wrt_parameter_limits(self):
+        # check initial parameter values are inside range
+        if (self.initial_value < self.lower_limits[self.parind[:]]).any() or (self.initial_value > self.upper_limits[self.parind[:]]).any():
+            # proposed value outside parameter limits
+            sys.exit('Proposed value outside parameter limits - select new initial parameter values')
             
-        return params
+    def check_prior_sigma(self, verbosity):
+        self.message(verbosity, 2, 'If prior variance <= 0, setting to Inf\n')
+        self.thetasigma = self.replace_list_elements(self.thetasigma, self.less_than_or_equal_to_zero, float('Inf'))
+        
+    def less_than_or_equal_to_zero(self, x):
+        return (x<=0)
+
+    def replace_list_elements(self, x, testfunction, value):
+        for ii in range(len(x)):
+            if testfunction(x[ii]):
+                x[ii] = value
+        return x
+    
+    def message(self, verbosity, level, printthis):
+        printed = False
+        if verbosity >= level:
+            print(printthis)
+            printed = True
+        return printed
