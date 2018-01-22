94e0f1b7f158db2fbafd4df86a23d398348e09d5
prmiles <prmiles@ncsu.edu>
2018-01-22 11:57:08 -0500

    modified class to improve accessibility
    - user will be able to see main features
    - non-user features are now denoted with single underscore

--- pymcmcstat/SimulationOptions.py
+++ pymcmcstat/SimulationOptions.py
@@ -54,9 +54,11 @@ class SimulationOptions:
     
     def __init__(self):
         # initialize simulation option variables
-        self.options = BaseSimulationOptions()
+#        self.options = BaseSimulationOptions()
+        self.description = 'simulation_options'
+        self.__options_set = False
         
-    def update_simulation_options(self, nsimu=10000, adaptint = None, ntry = None, method='dram',
+    def define_simulation_options(self, nsimu=10000, adaptint = None, ntry = None, method='dram',
                  printint=np.nan, adaptend = 0, lastadapt = 0, burnintime = 0,
                  waitbar = 1, debug = 0, qcov = None, updatesigma = 0, 
                  noadaptind = [], stats = 0, drscale = np.array([5, 4, 3], dtype = float),
@@ -77,123 +79,123 @@ class SimulationOptions:
         
         # define items from dictionary
         if adaptint is None:
-            self.options.adaptint = method_dictionary[method]['adaptint']  # update interval for adaptation
+            self.adaptint = method_dictionary[method]['adaptint']  # update interval for adaptation
         elif method == 'mh' or method == 'dr':
-            self.options.adaptint = method_dictionary[method]['adaptint']  # no adaptation - enforce!
+            self.adaptint = method_dictionary[method]['adaptint']  # no adaptation - enforce!
         else:
-            self.options.adaptint = adaptint
+            self.adaptint = adaptint
         
         if ntry is None:
-            self.options.ntry = method_dictionary[method]['ntry']
+            self.ntry = method_dictionary[method]['ntry']
         else:
-            self.options.ntry = ntry
+            self.ntry = ntry
             
         if adascale is None:
-            self.options.adascale = method_dictionary[method]['adascale']  # qcov_scale
+            self.adascale = method_dictionary[method]['adascale']  # qcov_scale
         else:
-            self.options.adascale = adascale
+            self.adascale = adascale
             
         if doram is None:
-            self.options.doram = method_dictionary[method]['doram']
+            self.doram = method_dictionary[method]['doram']
         else:
-            self.options.doram = doram
+            self.doram = doram
         
         
-        self.options.nsimu = nsimu  # length of chain to simulate
-        self.options.method = method
+        self.nsimu = nsimu  # length of chain to simulate
+        self.method = method
         
-        self.options.printint = printint  # print interval
-        self.options.adaptend = adaptend  # last adapt
-        self.options.lastadapt = lastadapt # last adapt
-        self.options.burnintime = burnintime
-        self.options.waitbar = waitbar # use waitbar
-        self.options.debug = debug  # show some debug information
-        self.options.qcov = qcov  # proposal covariance
-        self.options.initqcovn = initqcovn  # proposal covariance weight in update
-        self.options.updatesigma = updatesigma  # 
-        self.options.noadaptind = noadaptind  # do not adapt these indices
-        self.options.priorupdatestart = priorupdatestart
-        self.options.qcov_adjust = qcov_adjust  # eps adjustment
-        self.options.burnin_scale = burnin_scale
-        self.options.alphatarget = alphatarget  # acceptance ratio target
-        self.options.etaparam = etaparam  #
-        self.options.stats = stats  # convergence statistics
-        self.options.drscale = drscale
+        self.printint = printint  # print interval
+        self.adaptend = adaptend  # last adapt
+        self.lastadapt = lastadapt # last adapt
+        self.burnintime = burnintime
+        self.waitbar = waitbar # use waitbar
+        self.debug = debug  # show some debug information
+        self.qcov = qcov  # proposal covariance
+        self.initqcovn = initqcovn  # proposal covariance weight in update
+        self.updatesigma = updatesigma  # 
+        self.noadaptind = noadaptind  # do not adapt these indices
+        self.priorupdatestart = priorupdatestart
+        self.qcov_adjust = qcov_adjust  # eps adjustment
+        self.burnin_scale = burnin_scale
+        self.alphatarget = alphatarget  # acceptance ratio target
+        self.etaparam = etaparam  #
+        self.stats = stats  # convergence statistics
+        self.drscale = drscale
 
-        self.options.skip = skip
+        self.skip = skip
         
         if label is None:
-            self.options.label = str('MCMC run at {}'.format(datetime.now().strftime("%Y%m%d_%H%M%S")))
+            self.label = str('MCMC run at {}'.format(datetime.now().strftime("%Y%m%d_%H%M%S")))
         else:
-            self.options.label = label
+            self.label = label
             
-        self.options.RDR = RDR
-        self.options.verbosity = verbosity # amount of information to print
-        self.options.maxiter = maxiter
-        self.options.savesize = savesize
-        self.options.maxmem = maxmem
-        self.options.chainfile = chainfile
-        self.options.s2chainfile = s2chainfile
-        self.options.sschainfile = sschainfile
-        self.options.savedir = savedir
+        self.RDR = RDR
+        self.verbosity = verbosity # amount of information to print
+        self.maxiter = maxiter
+        self.savesize = savesize
+        self.maxmem = maxmem
+        self.chainfile = chainfile
+        self.s2chainfile = s2chainfile
+        self.sschainfile = sschainfile
+        self.savedir = savedir
         
-        self.options.rndseq = rndseq # define random number sequence for testing
+        self.__options_set = True # options have been defined
         
-    def check_dependent_simulation_options(self, data, model):
+    def _check_dependent_simulation_options(self, data, model):
         # check dependent parameters
                 
         # save options
-        if self.options.savesize <= 0 or self.options.savesize > self.options.nsimu:
-            self.options.savesize = self.options.nsimu
+        if self.savesize <= 0 or self.savesize > self.nsimu:
+            self.savesize = self.nsimu
         
         # turn on DR if ntry > 1
-        if self.options.ntry > 1:
-            self.options.dodram = 1
+        if self.ntry > 1:
+            self.dodram = 1
         else:
-            self.options.dodram = 0
+            self.dodram = 0
             
-        if self.options.lastadapt < 1:
-            self.options.lastadapt = self.options.nsimu
+        if self.lastadapt < 1:
+            self.lastadapt = self.nsimu
             
-        if np.isnan(self.options.printint):
-            self.options.printint = max(100,min(1000,self.options.adaptint))
+        if np.isnan(self.printint):
+            self.printint = max(100,min(1000,self.adaptint))
             
         # if N0 given, then also turn on updatesigma
         if model.N0 is not None:
-            self.options.updatesigma = 1  
+            self.updatesigma = 1  
             
     def display_simulation_options(self):
         print_these = ['nsimu', 'adaptint', 'ntry', 'method', 'printint', 'lastadapt', 'drscale']
-        print('simulation_options:')
+        print('simulation options:')
         for ii in xrange(len(print_these)):
-            print('\t{} = {}'.format(print_these[ii], getattr(self.options, print_these[ii])))
+            print('\t{} = {}'.format(print_these[ii], getattr(self, print_these[ii])))
             
-class BaseSimulationOptions:
-    def __init__(self):
-        self.nsimu = 10000
-        self.adaptint = None
-        self.ntry = None
-        self.method = 'dram'
-        self.printint = np.nan
-        self.adaptend = 0
-        self.lastadapt = 0
-        self.burnintime = 0
-        self.noadaptind = []
-        self.stats = 0
-        self.drscale = np.array([5,4,3], dtype = float)
-        self.adascale = None
-        self.savesize = 0
-        self.maxmem = 0
-        self.chainfile = None
-        self.s2chainfile = None
-        self.sschainfile = None
-        self.savedir = None
-        self.skip = 1
-        self.priorupdatestart = 0
-        self.qcov_adjust = 1e-8
-        self.burnin_scale = 10
-        self.alphatarget = 0.234
-        self.etaparam = 0.7
-        self.initqcovn = None
-        self.doram = None
-        self.rndseq = None
+#class BaseSimulationOptions:
+#    def __init__(self):
+#        self.nsimu = 10000
+#        self.adaptint = None
+#        self.ntry = None
+#        self.method = 'dram'
+#        self.printint = np.nan
+#        self.adaptend = 0
+#        self.lastadapt = 0
+#        self.burnintime = 0
+#        self.noadaptind = []
+#        self.stats = 0
+#        self.drscale = np.array([5,4,3], dtype = float)
+#        self.adascale = None
+#        self.savesize = 0
+#        self.maxmem = 0
+#        self.chainfile = None
+#        self.s2chainfile = None
+#        self.sschainfile = None
+#        self.savedir = None
+#        self.skip = 1
+#        self.priorupdatestart = 0
+#        self.qcov_adjust = 1e-8
+#        self.burnin_scale = 10
+#        self.alphatarget = 0.234
+#        self.etaparam = 0.7
+#        self.initqcovn = None
+#        self.doram = None
+#        self.rndseq = None
