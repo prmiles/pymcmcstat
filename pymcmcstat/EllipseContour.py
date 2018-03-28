e4e5a2bce6ca492a54bfc6a2fb3bdaf6c6b4f991
prmiles <prmiles@ncsu.edu>
2018-03-28 10:36:36 -0400

    cleaned up documentation in package
    - added check for positive definite matrix

--- pymcmcstat/EllipseContour.py
+++ pymcmcstat/EllipseContour.py
@@ -14,17 +14,9 @@ class EllipseContour:
     def __init__(self):
         self.description = 'Generates a probability contour ellipse'
 
-    def generate_ellipse(self, mu, cmat):
+    def generate_ellipse(self, mu, cmat, ndp = 100):
         '''
-        %ELLIPSE Draws a probability contour ellipse
-        % ellipse(mu,cmat) draws an ellipse centered at mu and with
-        % covariance matrix cmat.
-        % h = ellipse(...) plots and returns handle to the ellipse line
-        % [x,y] = ellipse(...) returns ellipse points (no plot)
-        % use chiqf(0.9,2)*cmat to get 90% Gaussian probability region
-        % example:
-        % plot(x(:,1),x(:,2),'.'); % plot data points
-        % hold on; ellipse(xmean, chiqf(0.9,2)*xcov); hold off
+        % Generates points for a probability contour ellipse
         
         % Marko Laine <Marko.Laine@Helsinki.FI>
         % $Revision: 1.6 $  $Date: 2007/08/10 08:50:40 $
@@ -34,15 +26,18 @@ class EllipseContour:
         
         # check shape of covariance matrix
         if cmat.shape != (2,2):
-            sys.exit('covmat must be 2x2')
+            sys.exit('covariance matrix must be 2x2')
         
         if self.__check_symmetric(cmat) is not True:
-            sys.exit('covmat must be symmetric')
+            sys.exit('covariance matrix must be symmetric')
             
         
         # define t space
-        t = np.linspace(0, 2*pi, 100)
-        R = np.linalg.cholesky(cmat).T
+        t = np.linspace(0, 2*pi, ndp)
+        
+        pdflag, R = self.__is_semi_pos_def_chol(cmat)
+        if pdflag is False:
+            sys.exit('covariance matrix must be positive definite')
         
         x = mu[0] + R[0,0]*cos(t)
         y = mu[1] + R[0,1]*cos(t) + R[1,1]*sin(t)
@@ -51,3 +46,11 @@ class EllipseContour:
     
     def __check_symmetric(self, a, tol=1e-8):
         return np.allclose(a, a.T, atol = tol)
+    
+    def __is_semi_pos_def_chol(self, x):
+        c = None
+        try:
+            c = np.linalg.cholesky(x)
+            return True, c.transpose()
+        except np.linalg.linalg.LinAlgError:
+            return False, c
