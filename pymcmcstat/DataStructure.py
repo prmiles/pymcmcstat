37402d90f779e85dadfac0b00712e8cef22b685a
prmiles <prmiles@ncsu.edu>
2018-04-11 13:04:46 -0400

    added conversion method to make x, y 2d numpy arrays

--- pymcmcstat/DataStructure.py
+++ pymcmcstat/DataStructure.py
@@ -31,6 +31,10 @@ class DataStructure:
         x = self.__convert_to_numpy_array(x)
         y = self.__convert_to_numpy_array(y)
         
+        # convert to 2d arrays (if applicable)
+        x = self.__convert_numpy_array_to_2d(x)
+        y = self.__convert_numpy_array_to_2d(y)
+        
         # append new data set
         self.xdata.append(x)
         self.ydata.append(y)
@@ -38,7 +42,9 @@ class DataStructure:
         if n is None:
             if isinstance(y, list): # y is a list
                 self.n.append(len(y))
-            else:
+            elif isinstance(y, np.ndarray) and y.size == 1:
+                self.n.append(y.size)
+            else: # should 
                 self.n.append(y.shape[0]) # assume y is a numpy array - nrows is n
         
         self.shape.append(y.shape)
@@ -49,7 +55,14 @@ class DataStructure:
         
     def __convert_to_numpy_array(self, xy):
         if isinstance(xy, np.ndarray) is False:
-            xy = np.array(xy)
+            xy = np.array([xy])
+            
+        return xy
+    
+    def __convert_numpy_array_to_2d(self, xy):
+        if xy.ndim != 2: # numpy array is (xy.size,) -> Convert to (xy.size,1)
+            xy = xy.reshape(xy.size,1)
+            
         return xy
         
     def check_data_type(self, xy):
@@ -70,7 +83,7 @@ class DataStructure:
         ncols = []
         for ii in range(ndatabatches):
             nrows.append(dshapes[ii][0])
-            if len(dshapes[0]) != 1:
+            if len(dshapes[ii]) != 1:
                 ncols.append(dshapes[ii][1])
         
         self.ndatasets = sum(ncols)
