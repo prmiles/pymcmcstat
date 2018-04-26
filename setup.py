448f0d26bd29d0b1c2bc88a3e4c142d726e89b82
prmiles <prmiles@ncsu.edu>
2018-04-26 08:17:36 -0400

    modified setup and init files to reference version file

--- setup.py
+++ setup.py
@@ -1,7 +1,19 @@
 from setuptools import setup
+import codecs
+
+def read(fname):
+    with codecs.open(fname, 'r', 'latin') as f:
+        return f.read()
+    
+# read in version number
+version_dummy = {}
+exec(read('pymcmcstat/__version__.py'), version_dummy)
+__version__ = version_dummy['__version__']
+del version_dummy
+
 setup(
     name='pymcmcstat',
-    version='1.4.0',
+    version=__version__,
     description='A library to perform mcmc simulations',
     url='https://github.com/prmiles/pymcmcstat',
     author='Paul Miles',
