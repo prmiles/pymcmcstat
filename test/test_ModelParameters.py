7968c3c92f43f1679dcd48072de09a33b135e4aa
prmiles <prmiles@ncsu.edu>
2018-05-23 07:58:31 -0400

    updated function calls in test routine
    - input arguments were updated in display metho

--- test/test_ModelParameters.py
+++ test/test_ModelParameters.py
@@ -44,10 +44,10 @@ class Add_Model_Parameter_Test(unittest.TestCase):
         # initialize default options
         SO = SimulationOptions()
         SO.define_simulation_options()
-        MP.display_parameter_settings(SO)
+        MP.display_parameter_settings(verbosity = SO.verbosity, noadaptind = SO.noadaptind)
         MP._results_to_params(results, 1)
         MP._openparameterstructure(nbatch=1)
-        MP.display_parameter_settings(SO)
+        MP.display_parameter_settings(verbosity = SO.verbosity, noadaptind = SO.noadaptind)
         self.assertEqual(MP.parameters[0]['theta0'], results['theta'][0])
         
 #    def test_does_non_square_matrix_return_error(self):
