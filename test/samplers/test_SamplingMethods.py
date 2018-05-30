31f68e3bf73a3b344b1c1ea60356b0321b2b2a95
prmiles <prmiles@ncsu.edu>
2018-05-30 09:54:28 -0400

    added test file for SamplingMethods class

---
+++ test/samplers/test_SamplingMethods.py
@@ -0,0 +1,23 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+"""
+Created on Wed May 30 09:47:02 2018
+
+@author: prmiles
+"""
+from pymcmcstat.samplers.SamplingMethods import SamplingMethods
+from pymcmcstat.samplers.Metropolis import Metropolis
+from pymcmcstat.samplers.DelayedRejection import DelayedRejection
+from pymcmcstat.samplers.Adaptation import Adaptation
+
+import unittest
+
+# -------------------
+class CreateSamplingMethods(unittest.TestCase):
+    
+    def test_create_sampling_methods(self):
+        SM = SamplingMethods()
+        self.assertTrue(isinstance(SM.metropolis, Metropolis), msg = 'Metropolis Class')
+        self.assertTrue(isinstance(SM.delayed_rejection, DelayedRejection), msg = 'Metropolis Class')
+        self.assertTrue(isinstance(SM.adaptation, Adaptation), msg = 'Metropolis Class')
+        
