66dee726a4cdf9d4f50caeb46266c8ab8bd270d9
prmiles <prmiles@ncsu.edu>
2018-05-31 11:04:39 -0400

    added test file for NumpyEncoder module

---
+++ test/utilities/test_NumpyEncoder.py
@@ -0,0 +1,25 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+"""
+Created on Wed May 30 07:34:40 2018
+
+@author: prmiles
+"""
+from pymcmcstat.utilities.NumpyEncoder import NumpyEncoder
+import unittest
+import numpy as np
+
+NE = NumpyEncoder()
+
+# --------------------------
+class NumpyEncoderTesting(unittest.TestCase):
+
+    def test_NE_initialization_attributes(self):
+        self.assertTrue(hasattr(NE, 'default'))
+        
+    def test_NE_return_ndarray(self):
+        self.assertTrue(isinstance(NE.default(obj = np.ones([10,1])), list), msg = 'Numpy array converted to list')
+        
+    def test_NE_return_json(self):
+        with self.assertRaises(TypeError, msg = 'dict not JSON serializable'):
+            NE.default(obj = {'test_object': [10,1]})
