0f9f7ece1a875f6e2c847a527c4d67427a70447e
prmiles <prmiles@ncsu.edu>
2018-06-26 06:29:43 -0400

    added general utility functions

---
+++ pymcmcstat/utilities/general.py
@@ -0,0 +1,24 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+"""
+Created on Tue Jun 26 06:16:02 2018
+
+General functions used throughout package
+
+@author: prmiles
+"""
+
+def message(verbosity, level, printthis):
+    '''
+    Display message
+    
+    :Args:
+        * **verbosity** (:py:class:`int`): Verbosity of display output.
+        * **level** (:py:class:`int`): Print level relative to verbosity.
+        * **printthis** (:py:class:`str'): String to be printed.
+    '''
+    printed = False
+    if verbosity >= level:
+        print(printthis)
+        printed = True
+    return printed
