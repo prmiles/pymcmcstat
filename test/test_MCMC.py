e64224659d371cedfa8bf2b7c9e02d8ef8bf96c9
prmiles <prmiles@ncsu.edu>
2018-06-29 16:37:08 -0400

    reduced duplication in MCMC test file

--- test/test_MCMC.py
+++ test/test_MCMC.py
@@ -6,57 +6,16 @@ Created on Wed Jun  6 08:33:47 2018
 @author: prmiles
 """
 
-from pymcmcstat.MCMC import print_rejection_statistics
-from pymcmcstat.MCMC import MCMC
+from pymcmcstat.MCMC import print_rejection_statistics, MCMC
 from pymcmcstat.structures.ParameterSet import ParameterSet
+import test.general_functions as gf
 import unittest
 import io
 import sys
 import numpy as np
 
-# define test model function
-def modelfun(xdata, theta):
-    m = theta[0]
-    b = theta[1]
-    nrow = xdata.shape[0]
-    y = np.zeros([nrow,1])
-    y[:,0] = m*xdata.reshape(nrow,) + b
-    return y
-
-def ssfun(theta, data, local = None):
-    xdata = data.xdata[0]
-    ydata = data.ydata[0]
-    # eval model
-    ymodel = modelfun(xdata, theta)
-    # calc sos
-    ss = sum((ymodel[:,0] - ydata[:,0])**2)
-    return ss
-
-def setup_mcmc(initialize = True):
-    # Initialize MCMC object
-    mcstat = MCMC()
-    # Add data
-    nds = 100
-    x = np.linspace(2, 3, num=nds)
-    y = 2.*x + 3. + 0.1*np.random.standard_normal(x.shape)
-    mcstat.data.add_data_set(x, y)
-
-    mcstat.simulation_options.define_simulation_options(nsimu = int(5.0e3), updatesigma = 1, method = 'dram', verbosity = 0)
-
-    # update model settings
-    mcstat.model_settings.define_model_settings(sos_function = ssfun)
-    
-    mcstat.parameters.add_model_parameter(name = 'm', theta0 = 2., minimum = -10, maximum = np.inf, sample = 1)
-    mcstat.parameters.add_model_parameter(name = 'b', theta0 = -5., minimum = -10, maximum = 100, sample = 0)
-    mcstat.parameters.add_model_parameter(name = 'b2', theta0 = -5., minimum = -10, maximum = 100, sample = 1)
-    
-    if initialize:
-        mcstat._initialize_simulation()
-    
-    return mcstat
-
 def setup_pseudo_results(initialize = True):
-    mcstat = setup_mcmc(initialize = initialize)
+    mcstat = gf.setup_mcmc_case_cp(initialize = initialize)
     rejectedin = {'total': 10, 'in_adaptation_interval': 4, 'outside_bounds': 1}
     mcstat._MCMC__rejected = rejectedin.copy()
     mcstat._MCMC__simulation_time = 0.1
@@ -79,7 +38,7 @@ class MCMCInitialization(unittest.TestCase):
 class DisplayCurrentMCMCSettings(unittest.TestCase):
     
     def test_standard_print(self):
-        mcstat = setup_mcmc()
+        mcstat = gf.setup_mcmc_case_cp()
         capturedOutput = io.StringIO()                  # Create StringIO object
         sys.stdout = capturedOutput                     #  and redirect stdout.
         mcstat._MCMC__display_current_mcmc_settings()
@@ -105,7 +64,7 @@ class PrintRejectionStatistics(unittest.TestCase):
 # --------------------------
 class UpdateChain(unittest.TestCase):
     def test_chain_accepted(self):
-        mcstat = setup_mcmc()
+        mcstat = gf.setup_mcmc_case_cp()
         accept = 1
         outsidebounds = 0
         CL = {'theta':np.array([1.0, 2.0]), 'ss': 1.0, 'prior':0.0, 'sigma2': 0.0}
@@ -119,7 +78,7 @@ class UpdateChain(unittest.TestCase):
         self.assertEqual(mcstat._MCMC__old_set, parset, msg = 'old_set updated to new set')
         
     def test_chain_not_accepted_within_bounds(self):
-        mcstat = setup_mcmc()
+        mcstat = gf.setup_mcmc_case_cp()
         accept = 0
         outsidebounds = 0
         CL = {'theta':np.array([1.0, 2.0]), 'ss': 1.0, 'prior':0.0, 'sigma2': 0.0}
@@ -135,30 +94,32 @@ class UpdateChain(unittest.TestCase):
         
 # --------------------------
 class UpdateRejected(unittest.TestCase):
-    def test_update_rejection_stats_not_outsidebounds(self):
-        mcstat = setup_mcmc()
+    @classmethod
+    def common_setup(cls):
+        mcstat = gf.setup_mcmc_case_cp()
         rejectedin = {'total': 10, 'in_adaptation_interval': 4, 'outside_bounds': 1}
         mcstat._MCMC__rejected = rejectedin.copy()
-        mcstat._MCMC__update_rejected(outsidebounds = 0)
-        
+        return mcstat, rejectedin
+    
+    def common_checks(self, mcstat, rejectedin, expect = 0):
         self.assertEqual(mcstat._MCMC__rejected['total'], rejectedin['total']+1, msg = 'Adds one to counter')
         self.assertEqual(mcstat._MCMC__rejected['in_adaptation_interval'], rejectedin['in_adaptation_interval']+1, msg = 'Adds one to counter')
-        self.assertEqual(mcstat._MCMC__rejected['outside_bounds'], rejectedin['outside_bounds'], msg = 'This counter stays the same.')
+        self.assertEqual(mcstat._MCMC__rejected['outside_bounds'], rejectedin['outside_bounds'] + expect, msg = 'This counter stays the same.')
+
+    def test_update_rejection_stats_not_outsidebounds(self):
+        mcstat, rejectedin = self.common_setup()
+        mcstat._MCMC__update_rejected(outsidebounds = 0)
+        self.common_checks(mcstat, rejectedin)
         
     def test_update_rejection_stats_outsidebounds(self):
-        mcstat = setup_mcmc()
-        rejectedin = {'total': 10, 'in_adaptation_interval': 4, 'outside_bounds': 1}
-        mcstat._MCMC__rejected = rejectedin.copy()
+        mcstat, rejectedin = self.common_setup()
         mcstat._MCMC__update_rejected(outsidebounds = 1)
-        
-        self.assertEqual(mcstat._MCMC__rejected['total'], rejectedin['total']+1, msg = 'Adds one to counter')
-        self.assertEqual(mcstat._MCMC__rejected['in_adaptation_interval'], rejectedin['in_adaptation_interval']+1, msg = 'Adds one to counter')
-        self.assertEqual(mcstat._MCMC__rejected['outside_bounds'], rejectedin['outside_bounds']+1, msg = 'Adds one to counter')
+        self.common_checks(mcstat = mcstat, rejectedin = rejectedin, expect = 1)
         
 # --------------------------
 class InitializeChain(unittest.TestCase):
     def test_initialize_chain_updatesigma_1(self):
-        mcstat = setup_mcmc()
+        mcstat = gf.setup_mcmc_case_cp()
         CL = {'theta':np.array([1.0, 2.0]), 'ss': 1.0, 'prior':0.0, 'sigma2': 0.0}
         mcstat._MCMC__old_set = ParameterSet(theta = CL['theta'], ss = CL['ss'], prior = CL['prior'], sigma2 = CL['sigma2'])
         mcstat._MCMC__chain_index = mcstat.simulation_options.nsimu - 1
@@ -168,7 +129,7 @@ class InitializeChain(unittest.TestCase):
         self.assertEqual(mcstat._MCMC__s2chain.shape, (mcstat.simulation_options.nsimu, 1), msg = str('Shape should be (nsimu,1) -> {}'.format(mcstat._MCMC__s2chain.shape)))
         
     def test_initialize_chain_updatesigma_0(self):
-        mcstat = setup_mcmc()
+        mcstat = gf.setup_mcmc_case_cp()
         mcstat.simulation_options.updatesigma = 0
         CL = {'theta':np.array([1.0, 2.0]), 'ss': 1.0, 'prior':0.0, 'sigma2': 0.0}
         mcstat._MCMC__old_set = ParameterSet(theta = CL['theta'], ss = CL['ss'], prior = CL['prior'], sigma2 = CL['sigma2'])
@@ -179,7 +140,7 @@ class InitializeChain(unittest.TestCase):
         self.assertEqual(mcstat._MCMC__s2chain, None, msg = str('s2chain should be None -> {}'.format(mcstat._MCMC__s2chain)))
         
     def test_initialize_chain_updatesigma_1_nsos_2(self):
-        mcstat = setup_mcmc()
+        mcstat = gf.setup_mcmc_case_cp()
         mcstat.model_settings.nsos = 2
         CL = {'theta':np.array([1.0, 2.0]), 'ss': 1.0, 'prior':0.0, 'sigma2': 0.0}
         mcstat._MCMC__old_set = ParameterSet(theta = CL['theta'], ss = CL['ss'], prior = CL['prior'], sigma2 = CL['sigma2'])
@@ -192,7 +153,7 @@ class InitializeChain(unittest.TestCase):
 # --------------------------
 class ExpandChain(unittest.TestCase):
     def test_expand_chain_updatesigma_1(self):
-        mcstat = setup_mcmc()
+        mcstat = gf.setup_mcmc_case_cp()
         CL = {'theta':np.array([1.0, 2.0]), 'ss': 1.0, 'prior':0.0, 'sigma2': 0.0}
         mcstat._MCMC__old_set = ParameterSet(theta = CL['theta'], ss = CL['ss'], prior = CL['prior'], sigma2 = CL['sigma2'])
         mcstat._MCMC__chain_index = mcstat.simulation_options.nsimu - 1
@@ -203,7 +164,7 @@ class ExpandChain(unittest.TestCase):
         self.assertEqual(mcstat._MCMC__s2chain.shape, (mcstat.simulation_options.nsimu*2 - 1, 1), msg = str('Shape should be (nsimu,1) -> {}'.format(mcstat._MCMC__s2chain.shape)))
         
     def test_expand_chain_updatesigma_0(self):
-        mcstat = setup_mcmc()
+        mcstat = gf.setup_mcmc_case_cp()
         mcstat.simulation_options.updatesigma = 0
         CL = {'theta':np.array([1.0, 2.0]), 'ss': 1.0, 'prior':0.0, 'sigma2': 0.0}
         mcstat._MCMC__old_set = ParameterSet(theta = CL['theta'], ss = CL['ss'], prior = CL['prior'], sigma2 = CL['sigma2'])
@@ -215,7 +176,7 @@ class ExpandChain(unittest.TestCase):
         self.assertEqual(mcstat._MCMC__s2chain, None, msg = str('s2chain should be None -> {}'.format(mcstat._MCMC__s2chain)))
         
     def test_expand_chain_updatesigma_1_nsos_2(self):
-        mcstat = setup_mcmc()
+        mcstat = gf.setup_mcmc_case_cp()
         mcstat.model_settings.nsos = 2
         CL = {'theta':np.array([1.0, 2.0]), 'ss': 1.0, 'prior':0.0, 'sigma2': 0.0}
         mcstat._MCMC__old_set = ParameterSet(theta = CL['theta'], ss = CL['ss'], prior = CL['prior'], sigma2 = CL['sigma2'])
@@ -229,7 +190,7 @@ class ExpandChain(unittest.TestCase):
 # --------------------------
 class SetupSimulator(unittest.TestCase):
     def test_setup_simu_use_prev_false(self):
-        mcstat = setup_mcmc(initialize=False)
+        mcstat = gf.setup_mcmc_case_cp(initialize=False)
         mcstat._MCMC__setup_simulator(use_previous_results = False)
         self.assertEqual(mcstat._MCMC__chain_index, 0, msg = 'Chain index should be 0')
         self.assertEqual(mcstat._MCMC__chain.shape, (mcstat.simulation_options.nsimu, 2), msg = str('Shape should be (nsimu,2) -> {}'.format(mcstat._MCMC__chain.shape)))
@@ -237,7 +198,7 @@ class SetupSimulator(unittest.TestCase):
         self.assertEqual(mcstat._MCMC__s2chain.shape, (mcstat.simulation_options.nsimu, 1), msg = str('Shape should be (nsimu,1) -> {}'.format(mcstat._MCMC__s2chain.shape)))
         
     def test_setup_simu_use_prev_true_causes_error(self):
-        mcstat = setup_mcmc(initialize=False)
+        mcstat = gf.setup_mcmc_case_cp(initialize=False)
         with self.assertRaises(SystemExit, msg = 'No previous results exist'):
             mcstat._MCMC__setup_simulator(use_previous_results = True)
     
