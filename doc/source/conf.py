cdcfa5bf4314bfcd631f3556c39743b0e44265f3
Paul Miles <prmiles@ncsu.edu>
2018-07-09 15:07:29 -0400

    removed reference to bootstrap theme

--- doc/source/conf.py
+++ doc/source/conf.py
@@ -16,7 +16,6 @@ import os
 import sys
 sys.path.insert(0, os.path.abspath('../..'))
 #sys.path.insert(0, os.path.abspath('../../pymcmcstat'))
-import sphinx_bootstrap_theme
 
 # -- Project information -----------------------------------------------------
 
