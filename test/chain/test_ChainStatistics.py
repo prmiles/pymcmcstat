bc8abc0f7cfa804e9f55044b9bddbb8b8dbff0eb
prmiles <prmiles@ncsu.edu>
2018-08-14 12:27:05 -0400

    added tests for Gelman-Rubin diagnostics

--- test/chain/test_ChainStatistics.py
+++ test/chain/test_ChainStatistics.py
@@ -30,7 +30,7 @@ class Chainstats_Eval(unittest.TestCase):
     def test_cs_eval_with_no_chain(self):
         stats = CS.chainstats(chain = None, returnstats = True)
         self.assertTrue(isinstance(stats, str))
-        
+# --------------------------        
 class BatchMeanSTD(unittest.TestCase):
     
     def test_len_s(self):
@@ -40,7 +40,7 @@ class BatchMeanSTD(unittest.TestCase):
     def test_too_few_batches(self):
         with self.assertRaises(SystemExit, msg = 'too few batches'):
             CS.batch_mean_standard_deviation(chain, b = chain.shape[0])
-            
+# --------------------------            
 class PowerSpectralDensity(unittest.TestCase):
     
     def test_nfft_none_size(self):
@@ -63,3 +63,47 @@ class PowerSpectralDensity(unittest.TestCase):
         nfft = min(len(x),256)
         n2 = int(np.floor(nfft/2))
         self.assertEqual(n2, len(y))
+        
+# --------------------------
+class GelmanRubin(unittest.TestCase):
+    def setup_chains(self):
+        chains = []
+        for ii in range(4):
+            chains.append(np.concatenate((ii*np.linspace(0, 1, 1000).reshape(1000,1), ii*np.linspace(2.5, 3.3, 1000).reshape(1000,1)), axis = 1))
+        return chains
+    
+    def standard_check(self, psrf):
+        self.assertTrue(isinstance(psrf, dict), msg = 'Expect dictionary output')
+        check_these = ['B', 'W', 'V', 'R', 'neff']
+        for ii, ps in enumerate(psrf):
+            for ct in check_these:
+                self.assertTrue(ct in psrf[ps], msg = str('{} not in {}'.format(ct, ps)))
+    
+    def test_gelman_rubin(self):
+        chains = self.setup_chains()
+        psrf = CS.gelman_rubin(chains = chains)
+        self.standard_check(psrf)
+                
+    def test_gelman_rubin_with_names(self):
+        chains = self.setup_chains()
+        psrf = CS.gelman_rubin(chains = chains, names = ['a', 'b'])
+        self.standard_check(psrf)
+        
+    def test_gelman_rubin_raise_error(self):
+        chains = self.setup_chains()
+        for ii in range(len(chains)-1):
+            chains.pop(-1)
+        with self.assertRaises(ValueError, msg = 'Must have multiple chains'):
+            CS.gelman_rubin(chains = chains)
+            
+# --------------------------
+class PSRF(unittest.TestCase):
+    def test_calc_psrf(self):
+        x = np.concatenate((np.linspace(0, 1, 1000).reshape(1000,1), np.linspace(2.5, 3.3, 1000).reshape(1000,1)), axis = 1)
+        psrf = CS.calculate_psrf(x, nsimu = 1000, nchains = 2)
+        self.assertTrue(isinstance(psrf, dict), msg = 'Expect dictionary output')
+        self.assertAlmostEqual(psrf['R'], 8.001818935964492, places = 6, msg = str('R: {} neq {}'.format(psrf['R'], 8.001818935964492)))
+        self.assertAlmostEqual(psrf['B'], 2879.9999999999964, places = 6, msg = str('R: {} neq {}'.format(psrf['B'], 2879.9999999999964)))
+        self.assertAlmostEqual(psrf['W'], 0.06853867547894903, places = 6, msg = str('R: {} neq {}'.format(psrf['W'], 0.06853867547894903)))
+        self.assertAlmostEqual(psrf['V'], 4.388470136803464, places = 6, msg = str('R: {} neq {}'.format(psrf['V'], 4.388470136803464)))
+        self.assertAlmostEqual(psrf['neff'], 3.047548706113521, places = 6, msg = str('R: {} neq {}'.format(psrf['neff'], 3.047548706113521)))
