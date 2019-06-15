812b7f11822e2a05bf48c93f21f082b8b2ff498f
prmiles <prmiles@ncsu.edu>
2019-06-15 06:28:24 -0400

    added results sequence to save lightly

--- pymcmcstat/MCMC.py
+++ pymcmcstat/MCMC.py
@@ -113,8 +113,14 @@ class MCMC:
         # Generate Results
         self.__generate_simulation_results()
         if self.simulation_options.save_to_json is True:
-            if self.simulation_results.basic is True:  # check that results structure has been created
-                self.simulation_results.export_simulation_results_to_json_file(results=self.simulation_results.results)
+            # check that results structure has been created
+            if self.simulation_results.basic is True:
+                if self.simulation_options.save_lightly is True:
+                    self.simulation_results.export_lightly(
+                            results=self.simulation_results.results)
+                else:                       
+                    self.simulation_results.export_simulation_results_to_json_file(
+                            results=self.simulation_results.results)
         self.mcmcplot = MCMCPlotting.Plot()
         self.PI = PredictionIntervals()
         self.chainstats = ChainStatistics.chainstats
